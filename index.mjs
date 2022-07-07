49c7db76819e228ec7580270052f85be1a83d728
Bojan Šernek <bojan@distopiksound.com>
2022-07-07 11:35:21 +0200

    v1.0.2

--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
 import { Worker, Queue } from "bullmq";
 import { temporaryFileTask } from "tempy";
+import { nanoid } from "nanoid";
+import { serializeError } from "serialize-error";
 import Axios from "axios";
 import wmatch from "wildcard-match";
 import body_parser from "body-parser";
@@ -9,15 +11,14 @@ import child from "child_process";
 import fs from "fs";
 import Express from "express";
 import process from "process";
-import { resolve } from "path";
 
 const app = Express();
+const axios = Axios;
+const job_name = "waveform";
+const port = process.env.PORT || 3000;
 const connection = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
 });
-const axios = Axios;
-const job_name = "waveform";
-const PORT = process.env.PORT || 3000;
 
 const queue = new Queue(job_name, { connection });
 
@@ -27,28 +28,45 @@ const worker = new Worker(
     console.log("processing job");
     console.table({ ...job.data, id: job.id });
 
-    const { wav_url, peaks_url, notify_url } = job.data;
+    const {
+      input_url,
+      input_format,
+      output_url,
+      output_format,
+      channel_mode,
+      notify_url,
+      bit_depth,
+      context,
+    } = job.data;
     await temporaryFileTask(
-      async (wav_loc) => {
+      async (input_loc) => {
         await temporaryFileTask(
-          async (peaks_loc) => {
-            await download(wav_url, wav_loc);
-            await generate_peaks(wav_loc, peaks_loc);
-            await upload(peaks_loc, peaks_url);
+          async (output_loc) => {
+            await download(input_url, input_loc);
+            await generate_peaks(
+              input_loc,
+              input_format,
+              channel_mode,
+              output_format,
+              bit_depth,
+              output_loc
+            );
+            await upload(output_loc, output_url);
           },
-          { extension: "dat" }
+          { extension: output_format }
         );
       },
-      { extension: "wav" }
+      { extension: input_format }
     );
 
-    await notify(notify_url);
+    await notify(notify_url, job.id, context, null);
   },
   { concurrency: parseInt(process.env.CONCURRENCY || "10"), connection }
 );
 
 worker.on("failed", (job, error) => {
   console.log({ job, error });
+  notify(job.data.notify_url, job.id, job.data.context, error).catch(() => {});
 });
 
 async function upload(path, url) {
@@ -86,18 +104,42 @@ async function download(url, path) {
   });
 }
 
-function generate_peaks(wav_loc, peaks_loc) {
+function generate_peaks(
+  input_loc,
+  input_format,
+  channel_mode,
+  output_format,
+  bit_depth,
+  output_loc
+) {
   console.log("generating peaks");
-  console.table({ wav_loc, peaks_loc });
-  const awf = child.spawn("audiowaveform", [
-    "--split-channels",
+  console.table({
+    input_loc,
+    input_format,
+    channel_mode,
+    output_format,
+    bit_depth,
+    output_loc,
+  });
+
+  const args = [
+    "--input-format",
+    input_format,
     "--output-format",
-    "dat",
+    output_format,
+    "-b",
+    bit_depth,
     "-i",
-    wav_loc,
+    input_loc,
     "-o",
-    peaks_loc,
-  ]);
+    output_loc,
+  ];
+
+  if (channel_mode == "multi") {
+    args.unshift("--split-channels");
+  }
+
+  const awf = child.spawn("audiowaveform", args);
 
   return new Promise((resolve, reject) => {
     awf.on("exit", (code) => {
@@ -111,9 +153,15 @@ function generate_peaks(wav_loc, peaks_loc) {
   });
 }
 
-async function notify(url) {
-  console.log("notifying", url);
-  await axios.get(url);
+async function notify(url, id, context, err) {
+  console.log("notifying");
+  console.table({ url, id, context, err });
+
+  await axios.post(url, {
+    err: err ? serializeError(err) : null,
+    context,
+    id,
+  });
 }
 
 const is_domain_valid = wmatch(
@@ -128,23 +176,67 @@ app.use(body_parser.json());
 app.use(cors());
 
 app.post("/v1/create", (req, res, next) => {
-  const { wav_url, peaks_url, notify_url } = req.body;
-  if (!wav_url || !is_url_valid(wav_url)) {
-    throw new Error("input WAV URL is not valid");
+  const {
+    input_url,
+    input_format,
+    output_format,
+    output_url,
+    channel_mode,
+    bit_depth,
+    notify_url,
+    context,
+  } = req.body;
+  if (
+    input_format !== "wav" &&
+    input_format !== "flac" &&
+    input_format !== "mp3"
+  ) {
+    throw new Error("Input format is not valid");
   }
-  if (!peaks_url || !is_url_valid(peaks_url)) {
+
+  if (output_format !== "dat" && output_format !== "json") {
+    throw new Error("Output format is not valid");
+  }
+
+  if (!input_url || !is_url_valid(input_url)) {
+    throw new Error("Input URL is not valid");
+  }
+
+  if (!output_url || !is_url_valid(output_url)) {
     throw new Error("Output peaks URL is not valid");
   }
+
   if (!notify_url || !is_url_valid(notify_url)) {
     throw new Error("Notify URL is not valid");
   }
 
+  if (channel_mode !== "single" && channel_mode !== "multi") {
+    throw new Error("Channel mode is not valid");
+  }
+
+  if (bit_depth !== 8 && bit_depth !== 16) {
+    throw new Error("Bit depth is not valid");
+  }
+
   queue
-    .add(new Date().toISOString(), { wav_url, peaks_url, notify_url })
+    .add(
+      new Date().toISOString(),
+      {
+        input_url,
+        input_format,
+        output_format,
+        channel_mode,
+        bit_depth,
+        output_url,
+        notify_url,
+        context,
+      },
+      { jobId: nanoid() }
+    )
     .then((job) => res.json(job.id))
     .catch(next);
 });
 
-app.listen(PORT, () => {
-  console.log(`👋 Express application listening on port ${PORT}!`);
+app.listen(port, () => {
+  console.log(`👋 Express application listening on port ${port}!`);
 });
